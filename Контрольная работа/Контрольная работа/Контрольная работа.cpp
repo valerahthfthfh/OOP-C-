b9b2c7c781824474e3e80d23d93b79d327688704
Valera <vl176932@gmail.com>
2025-03-01 23:26:29 +0700

    Complited code

--- Контрольная работа/Контрольная работа/Контрольная работа.cpp
+++ Контрольная работа/Контрольная работа/Контрольная работа.cpp
@@ -2,6 +2,20 @@
 #include <string>
 using namespace std;
 
+/*
+* autor: Valera Leontev
+* 
+* brief: The code was changed and a 
+* new action was added in the form of
+* the user indicating the initial size
+* of the array, so that errors would not
+* be repeated as at the beginning of adding
+* objects to the array. the code was also
+* completely translated into the English
+* version for more convenient inspection
+* of the code by the teacher
+*/
+
 class Furniture {
 private:
     int weight;
@@ -31,12 +45,12 @@ public:
     }
 
     void OutputVoid() {
-        cout << "Размер: " << this->weight << endl
-            << "Цена: " << this->price << endl
-            << "Название: " << this->name << endl
-            << "Цвет: " << this->color << endl
-            << "Ширина: " << this->width << endl
-            << "Высота: " << this->height << endl;
+        cout << "Size: " << this->weight << endl
+            << "Price: " << this->price << endl
+            << "Name: " << this->name << endl
+            << "Color: " << this->color << endl
+            << "Width: " << this->width << endl
+            << "Height: " << this->height << endl;
     }
 
     void RegainAccess(int weight, int width, int height) {
@@ -45,7 +59,7 @@ public:
         this->height = height;
         this->price = weight * (width / height);
 
-        cout << "По введенным составляющим цена будет: " << this->price << endl;
+        cout << "For the entered components the price will be: " << this->price << endl;
     }
 
     float getPrice() const {
@@ -61,7 +75,7 @@ void sortFurniture(Furniture* objects[], int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (objects[j]->getPrice > objects[j + 1]->getPrice) {
-                // Меняем местами указатели
+                // Swap pointers
                 Furniture* temp = objects[j];
                 objects[j] = objects[j + 1];
                 objects[j + 1] = temp;
@@ -71,24 +85,27 @@ void sortFurniture(Furniture* objects[], int size) {
 }
 
 void MenuUser() {
-    const int arraySize = 5; // Максимальное количество объектов
-    Furniture* objects_Fool[arraySize]; // Массив указателей на объекты
-    int index = 0; // Индекс для отслеживания текущего количества объектов
+    int arraySize; // Максимальное количество объектов
+
+    cout << "Before starting work, indicate what size tablet you want? :";
+    cin >> arraySize;
+    Furniture* objects_Fool[arraySize]; // Array of pointers to objects
+    int index = 0; // Index to track the current number of objects
     bool isExit = false;
     int inputUser;
 
     while (!isExit) {
         cout << "__________________________________________________________________" << endl;
-        cout << "Выберите реализацию для работы" << endl;
-        cout << "[1] - Добавление пустого объекта к массиву\n"
-            << "[2] - Добавление объекта с данными, заполненными пользователем\n"
-            << "[3] - Редактирование любого поля\n"
-            << "[4] - Вывод в консоль информации обо всех объектах\n"
-            << "[5] - Сортировка массива\n"
-            << "[6] - Завершение работы программы" << endl;
+        cout << "Select an implementation to work with" << endl;
+        cout << "[1] - Adding an empty object to an array\n"
+            << "[2] - Adding an object with user - filled datan"
+            << "[3] - Editing any field\n"
+            << "[4] - Displaying information about all objects to the console\n"
+            << "[5] - Sort an array\n"
+            << "[6] - Terminating the program" << endl;
         cout << "__________________________________________________________________" << endl;
 
-        cout << "Ввод: ";
+        cout << "Add: ";
         cin >> inputUser;
 
         int weight, width, height;
@@ -98,66 +115,66 @@ void MenuUser() {
         switch (inputUser) {
         case 1:
             if (index < arraySize) {
-                objects_Fool[index] = new Furniture(); // Добавляем пустой объект
+                objects_Fool[index] = new Furniture(); // Adding an empty object
                 index++;
-                cout << "Пустой объект добавлен." << endl;
+                cout << "Empty object added." << endl;
             }
             else {
-                cout << "Массив полон!" << endl;
+                cout << "The array is full!" << endl;
             }
             break;
 
         case 2:
             if (index < arraySize) {
-                cout << "Введите размер объекта: ";
+                cout << "Enter object size: ";
                 cin >> weight;
-                cout << "Введите цену объекта: ";
+                cout << "Enter the price of the object: ";
                 cin >> price;
-                cout << "Введите название объекта: ";
+                cout << "Enter object name: ";
                 cin >> name;
-                cout << "Введите цвет объекта: ";
+                cout << "Enter object color: ";
                 cin >> color;
-                cout << "Введите ширину объекта: ";
+                cout << "Enter object wibth: ";
                 cin >> width;
-                cout << "Введите высоту объекта: ";
+                cout << "Enter object height: ";
                 cin >> height;
                 objects_Fool[index] = new Furniture(weight, price, name, color, width, height);
                 index++;
             }
             else {
-                cout << "Массив полон!" << endl;
+                cout << "The array is full!" << endl;
             }
             break;
 
         case 3:
             int editIndex;
-            cout << "Введите индекс объекта для редактирования (от 0 до " << index - 1 << "): ";
+            cout << "Enter the index of the object to edit(from 0 to " << index - 1 << "): ";
             cin >> editIndex;
 
             if (editIndex >= 0 && editIndex < index) {
-                cout << "Введите новый размер объекта: ";
+                cout << "Enter a new size for the object: ";
                 cin >> weight;
-                cout << "Введите новую цену объекта: ";
+                cout << "Enter a new price for the item ";
                 cin >> price;
-                cout << "Введите новое название объекта: ";
+                cout << "Enter a new name for the object: ";
                 cin >> name;
-                cout << "Введите новый цвет объекта: ";
+                cout << "Enter a new color for yhe object: ";
                 cin >> color;
-                cout << "Введите новую ширину объекта: ";
+                cout << "Enter a new wibth for the object: ";
                 cin >> width;
-                cout << "Введите новую высоту объекта: ";
+                cout << "Enter a new height for the object: ";
                 cin >> height;
 
-                delete objects_Fool[editIndex]; // Удаляем старый объект
-                objects_Fool[editIndex] = new Furniture(weight, price, name, color, width, height); // Создаем новый объект
+                delete objects_Fool[editIndex]; // Delete a old object
+                objects_Fool[editIndex] = new Furniture(weight, price, name, color, width, height); // Create a new object
             }
             else {
-                cout << "Неверный индекс!" << endl;
+                cout << "Invalid index!" << endl;
             }
             break;
 
         case 4:
-            cout << "Вывод всех объектов:" << endl;
+            cout << "Output oll for the object:" << endl;
             for (int i = 0; i < index; i++) {
                 if (objects_Fool[i] != nullptr) {
                     objects_Fool[i]->OutputVoid();
@@ -169,11 +186,11 @@ void MenuUser() {
         case 5:
             // Сортировка массива объектов по цене
             sortFurniture(objects_Fool, index);
-            cout << "Массив отсортирован по цене." << endl;
+            cout << "Array sorted by price." << endl;
             break;
 
         case 6:
-            cout << "Спасибо, что воспользовались программой!" << endl;
+            cout << "Thank you for using the program!" << endl;
             isExit = true;
             // Освобождаем память
             for (int i = 0; i < index; i++) {
@@ -182,7 +199,7 @@ void MenuUser() {
             break;
 
         default:
-            cout << "Вы ввели что-то не то! Попробуйте еще раз!" << endl;
+            cout << "You entered something wrong! try again!" << endl;
             break;
         }
     }
