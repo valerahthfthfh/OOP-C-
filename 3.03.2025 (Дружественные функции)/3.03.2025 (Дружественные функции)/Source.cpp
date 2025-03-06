c1cb5527b2c7c46fd3923ab2226c15a8d204af56
Valera <vl176932@gmail.com>
2025-03-06 21:40:31 +0700

    Good code

--- 3.03.2025 (Дружественные функции)/3.03.2025 (Дружественные функции)/Source.cpp
+++ 3.03.2025 (Дружественные функции)/3.03.2025 (Дружественные функции)/Source.cpp
@@ -2,21 +2,45 @@
 
 using namespace std;
 
-class class_twoo; 
+class class_twoo;
 
 class class_one {
+private:
+    int number_one;
+    int number_two;
 public:
-    int number_one = 5;
-    int number_two = 5;
+
+    class_one() {
+        this->number_one = 5;
+        this->number_two = 5;
+    }
+
+    class_one(int number_one, int number_two) {
+        this->number_one = number_one;
+        this->number_two = number_two;
+    }
 
     friend void SumNumberOll(class_one& value1, class_twoo& value2);
 };
 
 class class_twoo {
-public:
-    int number_tree = 6;
-    int number_four = 6;
+private:
+    int number_tree;
+    int number_four;
 
+public:
+
+    class_twoo() {
+        this->number_tree = 1;
+        this->number_four = 4;
+    }
+
+    class_twoo(int number_tree, int number_four) {
+        this->number_tree = number_tree;
+        this->number_four = number_four;
+    }
+
+    friend void SumNumberOll(class_one& value1, class_twoo& value2);
 };
 
 void SumNumberOll(class_one& value1, class_twoo& value2) {
